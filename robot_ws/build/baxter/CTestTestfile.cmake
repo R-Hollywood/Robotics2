7a08150d718937e6c0e3a755c2b864c63cafc447
Rachel-Anne Hollywood <2195303h@student.gla.ac.uk>
2018-01-25 16:52:03 +0000

    Robotics Lab 2 Baxter is a statue

---
+++ robot_ws/build/baxter/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/rflab/robot_ws/src/baxter
+# Build directory: /home/rflab/robot_ws/build/baxter
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
